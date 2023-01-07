5df7aa691561078522a2efa49193b8fb964f3f6c
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2023-01-07 19:07:30 +0000

    Update subqueries.sql

--- 3_Joining _Data/subqueries.sql
+++ 3_Joining _Data/subqueries.sql
@@ -201,7 +201,21 @@ WHERE year = 2015
 ORDER BY inflation_rate;
 
 
+-- Final Challenge
 
+-- Select fields from cities
+SELECT name, country_code, city_proper_pop, metroarea_pop, (city_proper_pop / metroarea_pop * 100) AS city_perc
+FROM cities
+-- Use subquery to filter city name
+WHERE name IN
+    (SELECT capital
+    FROM countries
+    WHERE (continent = 'Europe' OR continent LIKE '%America'))
+-- Add filter condition such that metroarea_pop does not have null values
+AND metroarea_pop IS NOT NULL
+-- Sort and limit the result
+ORDER BY city_perc DESC
+LIMIT 10;
 
 
 
