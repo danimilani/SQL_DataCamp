1d277760756f755d69f86355ce4ebda81d050188
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2023-01-07 14:22:25 +0000

    Update aggregate_fuctions.sql

--- aggregate_fuctions.sql
+++ aggregate_fuctions.sql
@@ -291,8 +291,61 @@ HAVING COUNT(title) > 10;
 -- WHERE filters individual records
 -- HAVING filters grouped records
 
+-- Filter with HAVING
 
+-- Select the country and distinct count of certification as certification_count
+SELECT country, COUNT(DISTINCT certification) AS certification_count
+FROM films
+-- Group by country
+GROUP BY country
+-- Filter results to countries with more than 10 different certifications
+HAVING COUNT(DISTINCT certification) > 10;
 
+-- HAVING and sorting
 
+-- Select the country and average_budget from films
+SELECT country, ROUND(AVG(budget), 2) AS average_budget
+FROM films
+-- Group by country
+GROUP BY country
+-- Filter to countries with an average_budget of more than one billion
+HAVING ROUND(AVG(budget), 2) > 1000000000
+-- Order by descending order of the aggregated budget
+ORDER BY average_budget DESC;
 
+-- All together now
+
+-- Select the release year for each film, filter for records released after 1990 and group by release year
+
+-- Select the budget for films released after 1990 grouped by year
+SELECT release_year
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year;
+
+-- Modify the query to also list the average budget and average gross
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year;
+
+-- Modify query
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+-- Modify the query to see only years with an avg_budget of more than 60 million
+HAVING AVG(budget) > 60000000;
+
+-- Order
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000
+-- Order the results from highest to lowest average gross and limit to one
+ORDER BY avg_gross DESC
+LIMIT 1;
 
