ac1bd3512b80d2d03df26f76186ccaead4120784
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2023-01-09 19:48:47 +0000

    Update case.sql

--- 4_Data_Manipulation_/case.sql
+++ 4_Data_Manipulation_/case.sql
@@ -1,1 +1,215 @@
 -- CASE
+-- SQL version of IF... THEN... statement
+
+CASE WHEN x = 1 THEN 'a'
+     WHEN x = 2 THEN 'b'
+     ELSE 'c' END AS new_column
+
+SELECT
+  id,
+  home_goal,
+  away_goal,
+  CASE WHEN home_goal > away_goal THEN 'Home Team Win'
+  WHEN home_goal < away_goal THEN 'Away Team Win'
+  ELSE 'Tie' END AS outcome
+FROM match
+WHERE season = '2013/2014';
+
+-- EXERCISES
+
+-- Basic CASE statements
+
+SELECT
+	-- Select the team long name and team API id
+	team_long_name,
+	team_api_id
+FROM teams_germany
+-- Only include FC Schalke 04 and FC Bayern Munich
+WHERE team_long_name IN ('FC Schalke 04', 'FC Bayern Munich');
+
+-- Identify the home team as Bayern Munich, Schalke 04, or neither
+SELECT 
+    CASE WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
+         WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+         ELSE 'Other' END AS home_team,
+	COUNT(id) AS total_matches
+FROM matches_germany
+-- Group by the CASE statement alias
+GROUP BY home_team;
+
+-- CASE statements comparing column values
+
+SELECT 
+	-- Select the date of the match
+	date,
+	-- Identify home wins, losses, or ties
+	CASE WHEN home_goal > away_goal THEN 'Home win!'
+        WHEN home_goal < away_goal THEN 'Home loss :(' 
+        ELSE 'Tie' END AS outcome
+FROM matches_spain;
+
+
+SELECT 
+	m.date,
+	--Select the team long name column and call it 'opponent'
+	t.team_long_name AS opponent, 
+	-- Complete the CASE statement with an alias
+	CASE WHEN m.home_goal > m.away_goal THEN 'Home win!'
+        WHEN m.home_goal < m.away_goal THEN 'Home loss :('
+        ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Left join teams_spain onto matches_spain
+LEFT JOIN teams_spain AS t
+ON m.awayteam_id = t.team_api_id;
+
+
+SELECT 
+	m.date,
+	t.team_long_name AS opponent,
+    -- Complete the CASE statement with an alias
+	CASE WHEN m.home_goal > m.away_goal THEN 'Barcelona win!'
+        WHEN m.home_goal < m.away_goal THEN 'Barcelona loss :(' 
+        ELSE 'Tie' END AS outcome 
+FROM matches_spain AS m
+LEFT JOIN teams_spain AS t 
+ON m.awayteam_id = t.team_api_id
+-- Filter for Barcelona as the home team
+WHERE m.hometeam_id = 8634; 
+
+
+-- CASE statements comparing two column values part 2
+
+-- Select matches where Barcelona was the away team
+SELECT  
+	m.date,
+	t.team_long_name AS opponent,
+	CASE WHEN m.away_goal > m.home_goal THEN 'Barcelona win!'
+        WHEN m.away_goal < m.home_goal THEN 'Barcelona loss :(' 
+        ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Join teams_spain to matches_spain
+LEFT JOIN teams_spain AS t 
+ON m.hometeam_id = t.team_api_id
+WHERE m.awayteam_id = 8634;
+
+-- In CASE things get more complex
+-- If you want to test multiple logical conditions in a CASE statement, you can use 
+-- AND inside your WHEN cause
+
+SELECT date, hometeam_id, awayteam_id,
+  CASE WHEN hometeam_id = 8455 AND home_goal > away_goal
+        THEN 'Chelsea home win!'
+       WHEN awayteam_id = 8455 AND home_goal < away_goal
+        THEN 'Chelsea away win!'
+       ELSE 'Loss or tie :(' END AS outcome
+FROM match
+WHERE hometeam_id = 8455 OR awayteam_id = 8455;
+
+-- To specify the query, include the CASE statement in the WHERE clause
+
+-- Example to exclude NULL values
+
+SELECT date, season,
+ CASE WHEN hometeam_id = 8455 AND home_goal > away_goal
+        THEN 'Chelsea home win!'
+       WHEN awayteam_id = 8455 AND home_goal < away_goal
+        THEN 'Chelsea away win!' END AS outcome
+FROM match
+WHERE CASE WHEN hometeam_id = 8455 AND home_goal > away_goal
+        THEN 'Chelsea home win!'
+       WHEN awayteam_id = 8455 AND home_goal < away_goal
+        THEN 'Chelsea away win!' END IS NOT NULL;
+
+-- EXERCISES
+
+-- In CASE of rivalry
+
+SELECT 
+	date,
+	-- Identify the home team as Barcelona or Real Madrid
+	CASE WHEN hometeam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END AS home,
+    -- Identify the away team as Barcelona or Real Madrid
+	CASE WHEN awayteam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END AS away
+FROM matches_spain
+WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
+      AND (awayteam_id = 8633 OR hometeam_id = 8633);
+
+SELECT 
+	date,
+	CASE WHEN hometeam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END as home,
+	CASE WHEN awayteam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END as away,
+	-- Identify all possible match outcomes
+	CASE WHEN home_goal > away_goal AND hometeam_id = 8634 THEN 'Barcelona win!'
+        WHEN home_goal > away_goal AND hometeam_id = 8633 THEN 'Real Madrid win!'
+        WHEN home_goal < away_goal AND awayteam_id = 8634 THEN 'Barcelona win!'
+        WHEN home_goal < away_goal AND awayteam_id = 8633 THEN 'Real Madrid win!'
+        ELSE 'Tie!' END AS outcome
+FROM matches_spain
+WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
+      AND (awayteam_id = 8633 OR hometeam_id = 8633);
+
+
+-- Filtering your CASE statement
+
+-- Select team_long_name and team_api_id from team
+SELECT
+	team_long_name,
+	team_api_id
+FROM teams_italy
+-- Filter for team long name
+WHERE team_long_name = 'Bologna';
+
+-- Select the season and date columns
+SELECT 
+	season,
+	date,
+    -- Identify when Bologna won a match
+	CASE WHEN hometeam_id = 9857 
+        AND home_goal > away_goal 
+        THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 
+        AND away_goal > home_goal 
+        THEN 'Bologna Win' 
+		END AS outcome
+FROM matches_italy;
+
+-- Select the season, date, home_goal, and away_goal columns
+SELECT 
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE 
+-- Exclude games not won by Bologna
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
+		END IS NOT NULL;
+
+-- CASE WHEN with aggregate functions
+-- CASE statements are great for caegorizing, filtering and aggregating data
+
+-- COUNT
+-- How many home and away goals did Liverpool score in each season?
+
+SELECT
+  season,
+  COUNT(CASE WHEN hometeam_id = 8650
+        AND home_goal > away_goal
+        THEN id END) AS home_wins
+FROM match
+GROUP BY season;
+
+
+SELECT
+season,
+  COUNT(CASE WHEN hometeam_id = 8650 AND home_goal > away_goal
+        THEN id END) AS home_wins,
+  COUNT(CASE WHEN awayteam_id = 8650 AND away_goal > home_goal
+        THEN id END) AS away_wins
+FROM match
+GROUP BY season;
